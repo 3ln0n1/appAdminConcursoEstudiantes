063d8007fdf12c641b024288aac29649e511b001
otniel <palacioso@globalhitss.com>
2023-02-23 07:53:37 -0600

    environments

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
 
   apiURL:'https://10.85.3.21:9443/concursoestudiantes-web/api',
   URL_HOME: 'http://localhost:58953',
