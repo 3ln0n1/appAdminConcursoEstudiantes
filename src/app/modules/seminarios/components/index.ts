b67ba09c3c60ec5eb4e0b092a97b31cbc51b3a1a
otniel <palacioso@globalhitss.com>
2023-02-23 08:12:20 -0600

    se agrega  agregar-formulario.componen

--- src/app/modules/seminarios/components/index.ts
+++ src/app/modules/seminarios/components/index.ts
@@ -1,10 +1,13 @@
 import { SeminariosComponent } from './seminarios/seminarios.component';
 import { DocumentosFormularioSeminarioComponent } from './documentos-formulario-seminario/documentos-formulario-seminario.component';
 import { AgregarSeminarioComponent } from './agregar-seminario/agregar-seminario.component';
-export const components = [SeminariosComponent,DocumentosFormularioSeminarioComponent,AgregarSeminarioComponent];
+import { AgregarFormularioComponent } from './agregar-formulario/agregar-formulario.component';
+
+export const components = [SeminariosComponent,DocumentosFormularioSeminarioComponent,AgregarSeminarioComponent,AgregarFormularioComponent];
 
 export * from './seminarios/seminarios.component';
 export * from './documentos-formulario-seminario/documentos-formulario-seminario.component';
 export * from './agregar-seminario/agregar-seminario.component';
+export * from './agregar-formulario/agregar-formulario.component';
 
 
